0841515b27ffbf07e507d46c69c33d734bec1470
EduFreit4s <52899552+EduFreit4s@users.noreply.github.com>
2019-10-20 21:17:52 -0300

    estamos sozinhos ou não, as duas opções assustam

---
+++ LISTAS/lista_3/q7/q7.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "sensor.h"
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+    ifstream entrada;
+    vector <DadosSensor> data;
+    vector <int> numeros_temp;
+    int num;
+
+    entrada.open("input.txt", ios::in);
+
+    while(entrada >> num){              // Passo 1: Ler numeros do arquivo txt
+        numeros_temp.push_back(num);    // Envia para vector int temp
+    }
+
+    sort(numeros_temp.begin(), numeros_temp.end()); // Passo 2: Ordena esses números
+
+    for(int i = 0; i < numeros_temp.size(); i++){   // Passo 3
+
+        int val_temp, fre_temp;
+
+        val_temp = numeros_temp[i]; // <-- Aqui fica salvo o número (valor)
+
+        // Essa função conta a frequencia desse valor no vetor
+        fre_temp = count(numeros_temp.begin(),numeros_temp.end(),numeros_temp[i]);
+
+        // No argumento do push back, o construtor é invocado. Ele cria um objeto
+        // que prenche valor e frequencia. O push back leva esse objeto para data.
+        data.push_back(DadosSensor(val_temp, fre_temp));
+        // Essa sacada do construtor evita que seja necessário criar um objeto temporário.
+    }
+    
+    for(int i = 0; i < data.size(); i++) data[i].printDados(); // Final. Todos objetos são plotados na tela
+
+    system("pause");
+    return 0;
+}
