774ecd40db5039831a4ecaffd4b8381b57de9c3b
EduFreit4s <edufreitas.pb@gmail.com>
2019-11-04 10:01:55 -0300

    Add files via upload

--- LISTAS/lista_4/q7/q7.cpp
+++ LISTAS/lista_4/q7/q7.cpp
@@ -3,24 +3,25 @@
 #include <iomanip>
 #define KEY "sair"
 #define KEY2 0
-#define MENOR 1000000000
-#define MAIOR -1000000000
+#define MAIOR -1000000000000000
+#define MENOR 1000000000000000
 
 using namespace std;
 
 int main(){
 
-    map <string, map <string, map <string, map < int, float>>>> fatura;
+    map <string, map <string, double>> fatura;
+    
     string produto;
-    int quantidade, total, key = 1;
-    float valor, media = 0, menor, maior;
+    int quantidade = 1, total = 0, key = 1;
+    double valor = 1, media = 0, menor = MENOR, maior = MAIOR;
 
     while(true){
-        cout << "digite o nome do produto: ";
+        cout << "digite o nome do produto (ou ""sair"" p/ terminar): ";
         getline(cin, produto);
         if(produto == KEY) break;
 
-        cout << "digite a quantidade: ";
+        cout << "digite a quantidade (): ";
         cin >> quantidade;
         if(quantidade == KEY2) break;
 
@@ -28,32 +29,55 @@ int main(){
         cin >> valor;
         if(valor == KEY2) break;
 
-        fatura[produto]["quantidade"]["preco"][quantidade] = valor;
+        fatura[produto]["quantidade"] = quantidade;
+        fatura[produto]["valor"] = valor;
 
         cin.ignore();
     }
 
+    cout << setw(20) << left << "Produto" << setw(20) << left << "Quantidade" << setw(20) << left << "Preco" << endl;
+    for(auto i : fatura){
+        cout << setw(20) << left << i.first << setw(20) << left << fatura[i.first]["quantidade"] << setw(20) << left << fatura[i.first]["valor"] << endl;
+    }
+
+    valor = 0;
+    
 
     for(auto i : fatura){
-        for(auto j : fatura[i.first]["quantidade"]["preco"]){
-            total += j.first;
-            media = (media + j.second)/key;
-            key++;
+        total += fatura[i.first]["quantidade"];
+        media = (media+(fatura[i.first]["valor"]*fatura[i.first]["quantidade"]))/key;
+        valor += fatura[i.first]["valor"];
+        key++;
+    }
+
+    for(auto i : fatura){
+        if(fatura[i.first]["valor"] < menor) menor = fatura[i.first]["valor"];
+        if(fatura[i.first]["valor"] > maior) maior = fatura[i.first]["valor"];
+    }
+
+
+
+    
+    
+    for(auto k : fatura){
+        if(fatura[k.first]["valor"] == menor){
+            cout << "Menor preco: " << k.first << ", valor: " << fatura[k.first]["valor"] << endl;
+            break;
         }
     }
 
-     for(auto i : fatura){
-        for(auto j : fatura[i.first]["quantidade"]["preco"]){
-            if(j.second < MENOR){
-                menor = j.second;
-            }
 
-            if(j.second > MAIOR){
-                maior = j.second;
-            }
+
+    for(auto k : fatura){
+        if(fatura[k.first]["valor"] == maior){
+            cout << "Maior preco: " << k.first << ", valor: " << fatura[k.first]["valor"] << endl;
+            break;
         }
     }
 
+    cout << "Total de produtos: " << total << endl;
+    cout << "Media de precos: " << media << endl;
+    cout << "Total das compras: " << valor << endl;
 
     return 0;
 }
