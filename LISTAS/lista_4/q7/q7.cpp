a63b4a510bfb32ee5e05585a21690625becf4b48
EduFreit4s <edufreitas.pb@gmail.com>
2019-11-04 19:42:08 -0300

    Add files via upload

--- LISTAS/lista_4/q7/q7.cpp
+++ LISTAS/lista_4/q7/q7.cpp
@@ -1,83 +1,64 @@
 #include <iostream>
 #include <map>
-#include <iomanip>
 #define KEY "sair"
 #define KEY2 0
-#define MAIOR -1000000000000000
-#define MENOR 1000000000000000
+#define MAIOR -10000000
+#define MENOR 10000000
 
 using namespace std;
 
 int main(){
 
-    map <string, map <string, double>> fatura;
-    
-    string produto;
-    int quantidade = 1, total = 0, key = 1;
-    double valor = 1, media = 0, menor = MENOR, maior = MAIOR;
+    map<string,map<string,double>> fatura;
+    string produto; 
+    double valor = 0, total = 0, media = 0, quantidade = 0, menor = MENOR, maior = MAIOR;
 
     while(true){
         cout << "digite o nome do produto (ou ""sair"" p/ terminar): ";
         getline(cin, produto);
         if(produto == KEY) break;
 
-        cout << "digite a quantidade: ";
-        cin >> quantidade;
-        if(quantidade == KEY2) break;
+        cout << "digite a quantidade (ou ""0"" p/ terminar): ";
+        cin >> fatura[produto]["quantidade"];
+        if(fatura[produto]["quantidade"] == KEY2) break;
 
-        cout << "digite o valor da unidade: ";
-        cin >> valor;
-        if(valor == KEY2) break;
-
-        fatura[produto]["quantidade"] = quantidade;
-        fatura[produto]["valor"] = valor;
+        cout << "digite o valor da unidade (ou ""0"" p/ terminar): ";
+        cin >> fatura[produto]["valor"];
+        if(fatura[produto]["valor"] == KEY2) break;
 
         cin.ignore();
     }
 
-    cout << setw(20) << left << "Produto" << setw(20) << left << "Quantidade" << setw(20) << left << "Preco" << endl;
-    for(auto i : fatura){
-        cout << setw(20) << left << i.first << setw(20) << left << fatura[i.first]["quantidade"] << setw(20) << left << fatura[i.first]["valor"] << endl;
-    }
-
-    valor = 0;
-    
-
-    for(auto i : fatura){
+    for(auto i: fatura){
+        quantidade += fatura[i.first]["preco"]*fatura[i.first]["quantidade"];
         total += fatura[i.first]["quantidade"];
-        media = (media+(fatura[i.first]["valor"]*fatura[i.first]["quantidade"]))/key;
-        valor += fatura[i.first]["valor"]*fatura[i.first]["quantidade"];
-        key++;
+        if(fatura[i.first]["preco"]*fatura[i.first]["quantidade"] < menor){
+            menor = fatura[i.first]["preco"]*fatura[i.first]["quantidade"];
+        }
+        if(fatura[i.first]["preco"]*fatura[i.first]["quantidade"] > maior){
+            maior = fatura[i.first]["preco"]*fatura[i.first]["quantidade"];
+        }
+    }
+
+    media= valor/quantidade;
+
+    for(auto i: fatura){
+        if(fatura[i.first]["preco"]*fatura[i.first]["quantidade"] == menor){
+            cout << "O produto mais barato: "<<i.first << ", com valor total: "<<menor<<endl;
+            break;
+        }
     }
 
     for(auto i : fatura){
-        if(fatura[i.first]["valor"] < menor) menor = fatura[i.first]["valor"]*fatura[i.first]["valor"];
-        if(fatura[i.first]["valor"] > maior) maior = fatura[i.first]["valor"]*fatura[i.first]["valor"];
-    }
-
-
-
-    
-    
-    for(auto k : fatura){
-        if((fatura[k.first]["valor"]*fatura[k.first]["valor"]) == menor){
-            cout << "Menor preco total: " << k.first << ", total: " << fatura[k.first]["valor"]*fatura[k.first]["valor"] << endl;
+        if(fatura[i.first]["preco"]*fatura[i.first]["quantidade"] == maior){
+            cout<<"O produto mais caro: "<<i.first << ", com valor total: "<<maior<<endl;
             break;
         }
     }
 
-
-
-    for(auto k : fatura){
-        if((fatura[k.first]["valor"]*fatura[k.first]["valor"]) == maior){
-            cout << "Maior preco total: " << k.first << ", total: " << fatura[k.first]["valor"]*fatura[k.first]["valor"] << endl;
-            break;
-        }
-    }
-
-    cout << "Total de produtos: " << total << endl;
-    cout << "Media de precos: " << media << endl;
-    cout << "Total das compras: " << valor << endl;
+    cout<<"Quantidade total de produtos: "<<total<<endl;
+    cout<<"Valor total: "<<valor<<endl;
+    cout<<"Media de precos:"<<media;
 
     return 0;
 }
