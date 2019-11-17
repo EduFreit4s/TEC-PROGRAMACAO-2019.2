fde3257084b766aeb63ff12071270ddc82ccf326
EduFreit4s <edufreitas.pb@gmail.com>
2019-11-17 15:03:21 -0300

    Add files via upload

---
+++ something/return/Classes/tempo.h/tempo.h
@@ -0,0 +1,28 @@
+#ifndef TEMPO_H
+#define TEMPO_H
+
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+#include <time.h>
+
+using namespace std;
+
+int getempo(char x);
+
+int getDia();
+int getMes();
+int getAno();
+
+int getSegundo();
+int getMinuto();
+int getHora();
+
+string getData();
+string getTempo();
+
+#endif
+
+
+
+
